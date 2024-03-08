97ce6e72253345915a913a48320501d306453309
Kauã <kaua.bm@aluno.ifsc.edu.br>
2024-03-08 12:11:24 -0300

    Commit Moveis 2

--- Moveis1/contaClick/app/src/main/java/com/contaclick/MainActivity.java
+++ Moveis1/contaClick/app/src/main/java/com/contaclick/MainActivity.java
@@ -4,25 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
-    int i=0;
-    TextView textView;
+
+    EditText edMin, edMax;
+    TextView textViewResult;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = findViewById(R.id.tvContador);
+        edMin = findViewById(R.id.editTextMin);
+        edMax = findViewById(R.id.editTextMax);
+        textViewResult = findViewById(R.id.textView);
+    }
+
+    public  void randNumber(View v){
+        int max, min;
+        min = integer.parseInt(edMin.getText().toString());
+        max = integer.perseInt(edMax.getText().toString());
+        Random R = new Random();
+        r.nextInt(bound.max-min+1);
     }
     @Override
     protected void onStart() {
         super.onStart();
-        textView.setText("0");
-    }
-    public void contar(View v){
-        i++;
-        TextView tv = (TextView) v;
-        tv.setText(Integer.toString(i));
     }
 }
